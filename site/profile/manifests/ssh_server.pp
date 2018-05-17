8eeb9e156a3a9affb3c3c98d332a81f06b6330a8
DavidHromyk <38985997+DavidHromyk@users.noreply.github.com>
2018-05-17 11:51:30 -0400

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,13 @@
+class profile::ssh_server {
+        package {'openssh-server':
+                ensure => present,
+        }
+        service { 'sshd':
+                ensure => 'running',
+                enable => 'true',
+        }
+        ssh_authorized_key { 'root@localhost.localdomain':
+                ensure => present,
+                user   => 'root',
+                type   => 'ssh-rsa',
+                key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCY7z1MQZMr4pRmEfcLn7pcTP5F7IeWs+9Av3xUWf1Cjlk0YVEjmBJuXFnSMGiUn0rlNwgQEDvhk9KrKE0+NXLgtN0rWSHYlKC83IRzt/Ki9Ucg3QqV+396rqaTisv7qIhotc0VB45z0SKEv+CWr+tHhvs+4XzN6POElv3myA1GrH31iKQhCOdeOZW/6plrt50OET41M/mIyJ0L+DkSsS79OP7p7ejANJHPw3vQFAGGz3+Ii1ZhRjqGUwhPfcOZNzawqwZF99yJNUvCeXx9fpZlrlaX6oqoa6EbCnaHCnpktB2nAV3BaW6MCHhN60C+FLXsOt69dFEElN2pDKoHgP9D',
